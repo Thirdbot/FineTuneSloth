90200b3c8b2ae5b36d407c0d3a1723da950393e2
thirdbot <astroll656@gmail.com>
2026-03-09 05:10:41 +0700

    add load model directly from huggingface

--- inference/inference.py
+++ inference/inference.py
@@ -13,7 +13,7 @@ HF_TOKEN = os.getenv("HF_TOKEN")
 
 client = InferenceClient(base_url=ENDPOINT_URL, token=HF_TOKEN)
 
-
+# predict from endpoints
 def predict_label(title: str) -> str:
     response = client.text_generation(
         title,
@@ -50,9 +50,28 @@ for i, (question, expected) in enumerate(student_questions, 1):
     print(f"   Predicted : {prediction}  {status}")
 print("\n" + "="*80)
 
-# test endpoint
+# test endpoint from local
 Home_path = Path(__file__).parent.parent.absolute()
 full_model_path = Home_path / "full_model_weights"
 my_handler = EndpointHandler(path=full_model_path.as_posix())
-data = {"inputs": "ธนาคารแห่งประเทศไทยขึ้นอัตราดอกเบี้ยนโยบาย 0.25%"}
+data = {"inputs": "ดื่มน้ำขิงเช้าเย็นรักษามะเร็งได้ 100% แพทย์ยืนยัน"}
 print(my_handler(data))
+
+# Load model directly from huggingface
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+tokenizer = AutoTokenizer.from_pretrained("thirdExec/Qwen2.5-1.5B-Instruct-ThaiFakeNews-bnb-4bit")
+model = AutoModelForCausalLM.from_pretrained("thirdExec/Qwen2.5-1.5B-Instruct-ThaiFakeNews-bnb-4bit")
+
+#single prediction
+messages = [{"role": "user", "content": "ดื่มน้ำขิงเช้าเย็นรักษามะเร็งได้ 100% แพทย์ยืนยัน"}]
+
+inputs = tokenizer.apply_chat_template(
+		messages,
+		add_generation_prompt=True,
+		tokenize=True,
+		return_dict=True,
+		return_tensors="pt",
+	).to(model.device)
+outputs = model.generate(**inputs, max_new_tokens=40)
+print(tokenizer.decode(outputs[0][inputs["input_ids"].shape[-1]:],skip_special_tokens=True))
